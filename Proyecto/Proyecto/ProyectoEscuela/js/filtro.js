9d777424b7f2674984f43a6dc6dfddcc895197ae
alexka9 <96111422+alexka9@users.noreply.github.com>
2024-05-24 21:33:19 +0200

    Delete Proyecto/Proyecto/ProyectoEscuela directory

--- Proyecto/Proyecto/ProyectoEscuela/js/filtro.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.toggle-button').addEventListener('click', function() {
-    const formContainer = document.querySelector('.form-container');
-    formContainer.style.display = formContainer.style.display === 'block' ? 'none' : 'block';
-});
